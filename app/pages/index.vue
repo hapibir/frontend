7e62c33e273d891fd68f99b432c6edce88c402a7
SnO2WMaN <sno2wman.official@gmail.com>
2019-11-17 17:23:06 +0900

    ✨ Add Twitter auth by firebase

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,16 +1,25 @@
 <template>
   <div>
-    <p>OK</p>
+    <div @click="login">Login</div>
   </div>
 </template>
 
 <script lang="ts">
 import { createComponent } from '@vue/composition-api'
 
+import firebase from '~/firebase'
+
 export default createComponent({
   name: 'PageIndex',
+  components: {},
   setup() {
-    return {}
+    return {
+      login: () => {
+        firebase
+          .auth()
+          .signInWithRedirect(new firebase.auth.TwitterAuthProvider())
+      }
+    }
   }
 })
 </script>
