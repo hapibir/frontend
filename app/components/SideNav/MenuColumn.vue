bd7efb7815d391a73a735082feb0db526a1ebf7c
SnO2WMaN <sno2wman.official@gmail.com>
2019-11-30 18:14:38 +0900

    ✨ Add menu column

---
+++ app/components/SideNav/MenuColumn.vue
@@ -0,0 +1,86 @@
+<i18n locale="en" lang="yaml">
+  home: "Home"
+  settings: "Settings"
+</i18n>
+
+<i18n locale="ja" lang="yml">
+  home: "ホームに戻る"
+  settings: "設定"
+</i18n>
+
+<template>
+  <li class="column">
+    <div class="icon-wrap">
+      <FontAwesomeIcon :icon="icon" />
+    </div>
+    <div class="text-wrap">
+      <p v-t="message" />
+    </div>
+    <nuxt-link :to="link" class="link" />
+  </li>
+</template>
+
+<script lang="ts">
+import { createComponent } from '@vue/composition-api'
+
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faHome, faCog } from '@fortawesome/pro-solid-svg-icons'
+
+export default createComponent({
+  components: { FontAwesomeIcon },
+  props: {
+    message: { type: String, required: true },
+    link: { type: String, required: true }
+  },
+  setup(props) {
+    const icons = {
+      home: faHome,
+      settings: faCog
+    }
+    return {
+      icon: icons[props.message as keyof typeof icons]
+    }
+  }
+})
+</script>
+
+<style lang="scss" scoped>
+.column {
+  display: flex;
+  align-items: center;
+  height: 72px;
+  padding: 0 16px;
+  background: $bg-whity;
+  box-shadow: 0 2px 16px $shadow-color;
+  position: relative;
+  border-left: solid $accent-color;
+  border-width: 0;
+  transition: 0.05s border-width ease;
+  &:hover {
+    border-width: 12px;
+  }
+}
+
+.icon-wrap {
+  size: 48px;
+  font-size: 32px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-right: 16px;
+  color: $text-color;
+}
+
+.text-wrap {
+  p {
+    color: $text-color;
+    font-family: var(--sans-serif-ja);
+  }
+}
+
+.link {
+  position: absolute 0;
+  size: 100%;
+  z-index: 1;
+}
+</style>
--- app/components/SideNav/SideNav.vue
+++ app/components/SideNav/SideNav.vue
@@ -2,7 +2,10 @@
   <nav class="side-nav">
     <UserInfoBlock v-if="isLogin" />
     <div v-else></div>
-    <p>A</p>
+    <ul class="menu">
+      <MenuColumn message="home" link="/home" />
+      <MenuColumn message="settings" link="/settings" />
+    </ul>
   </nav>
 </template>
 
@@ -10,9 +13,10 @@
 import { createComponent, ref } from '@vue/composition-api'
 
 import UserInfoBlock from './UserInfoBlock.vue'
+import MenuColumn from './MenuColumn.vue'
 
 export default createComponent({
-  components: { UserInfoBlock },
+  components: { UserInfoBlock, MenuColumn },
   setup(_props, context) {
     return { isLogin: ref(context.root.$accessor.isLogin) }
   }
@@ -23,8 +27,15 @@ export default createComponent({
 .side-nav {
   width: 320px;
   height: 100vh;
-  background-color: $bg-whity;
+  background-color: white;
   box-shadow: 0 0 48px $shadow-color;
   overflow: hidden;
+  display: flex;
+  flex-direction: column;
+}
+
+.menu {
+  flex-grow: 2;
+  user-select: none;
 }
 </style>
